0dee9ae80d40c8c194b62f2cc8548005841d0942
Band-Aid-2000 <136314728+Band-Aid-2000@users.noreply.github.com>
2023-06-12 16:21:31 +0700

    Update Baotangtruyentranh.ts

--- src/Baotangtruyentranh/Baotangtruyentranh.ts
+++ src/Baotangtruyentranh/Baotangtruyentranh.ts
@@ -21,7 +21,7 @@ import {
 
 import { parseSearch, parseViewMore, isLastPage, decodeHTMLEntity } from "./BaotangtruyentranhParser"
 
-const DOMAIN = 'https://baotangtruyentranh.com/'
+const DOMAIN = 'https://baotangtruyenvip.com/'
 const method = 'GET'
 
 export const BaotangtruyentranhInfo: SourceInfo = {
